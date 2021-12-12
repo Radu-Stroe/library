e3530e287c015b5549f1998c95d32623d14c9d62
Radu <stroeradu03@gmail.com>
2021-12-12 19:24:42 +0200

    Implement local storage

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let myLybrary = [];
-
 class Book {
   constructor(title = 'Unknown', author = 'Unknown', pages = 0, isRead = 'false') {
     this.title = title;
@@ -9,17 +7,25 @@ class Book {
   }
 }
 
-// class Library {
-//   constructor(books = []) {
-//     this.books = books;
-//   }
+class Library {
+  constructor(books = []) {
+    this.books = books;
+  }
 
-//   addBookToLibrary = (book) => {
-//     this.books.push(book);
-//   }
-// }
+  addBook = (book) => {
+    this.books.push(book);
+  }
 
-// const library = new Library();
+  removeBook = (bookTitle) => {
+    this.books.forEach((book) => {
+      if(book.title === bookTitle) {
+        myLybrary.splice(myLybrary.indexOf(book), 1);
+      }
+    })
+  }
+}
+
+const library = new Library();
 
 //ELEMENTS
 
@@ -30,6 +36,8 @@ const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const isRead = document.querySelector('#isRead');
 const form = document.querySelector('#form');
+const cards = document.querySelector('.cards');
+
 
 //FUNCTIONS
 
@@ -38,54 +46,75 @@ const showHideForm = (zIndex = 1, visibility = 'visible') => {
   contentForm.style.visibility = visibility;
 }
 
-const addBookToLibrary = (book) => myLybrary.push(book);
-
-const removeBookFromLibrary = (bookTitle) => {
-  myLybrary.forEach((book) => {
-    if(book.title === bookTitle) {
-      myLybrary.splice(myLybrary.indexOf(book), 1);
-    }
-  })
-}
 const getBook = () => new Book(title.value, author.value, pages.value, isRead.checked);
+
+//LOCAL STORAGE
+
+const saveLibraryToLocalStorage = (library) => {
+  localStorage.setItem('library', JSON.stringify(library.books));
+  window.location = window.location;
+}
+
+const loadLibraryFromLocalStorage = () => {
+  library.books = [...JSON.parse(localStorage.getItem('library'))];
+  console.log(library.books);
+};
 
 //EVENT LISTENERS
 
 addBookButton.addEventListener('click', () => showHideForm());
+
 form.addEventListener('submit', function(event) {
   event.preventDefault();
-  addBookToLibrary(getBook());
+  library.addBook(getBook());
+  saveLibraryToLocalStorage(library);
   form.reset();
   showHideForm(-1, 'hidden');
 })
 
 //RENDER BOOKS
 
-myLybrary.forEach((book) => {
+loadLibraryFromLocalStorage();
+
+library.books.forEach((book) => {
+  const card = document.createElement('div');
+  const bookTitle = document.createElement('p');
+  const bookAuthor = document.createElement('p');
+  const bookPages = document.createElement('p');
+  const readBookButton = document.createElement('button');
+  const removeBookButton = document.createElement('button');
+
+  bookTitle.textContent = book.title;
+  bookAuthor.textContent = book.author;
+  bookPages.textContent = book.pages;
+  removeBookButton.textContent = 'Remove';
+
+  if (book.isRead) {
+    readBookButton.textContent = 'Read';
+    readBookButton.classList.add('green');
+  } else {
+    readBookButton.textContent = 'Not Read';
+    readBookButton.classList.add('red');
+  }
+
+  card.classList.add('card');
+  readBookButton.classList.add('read-btn');
+  removeBookButton.classList.add('remove-btn');
   
+  cards.appendChild(card);
+  card.appendChild(bookTitle);
+  card.appendChild(bookAuthor);
+  card.appendChild(bookPages);
+  card.appendChild(readBookButton);
+  card.appendChild(removeBookButton);
+
+  readBookButton.addEventListener('click', () => {
+    book.isRead = !book.isRead
+    console.log(book.isRead)
+  });
+  removeBookButton.addEventListener('click', () => library.removeBook(book.title));
 })
-console.log(myLybrary);
 
-
-// form.addEventListener("submit", function(e) {
-//     const data = Object.fromEntries(new FormData(form).entries());
-//     console.log(data);
-// })
-
-
-
-// function submitForm(formElement) {
-//     const formData = new FormData(formElement);
-//     const allEntries = [...formData.entries()]
-//       .reduce((all, entry) => {
-//         all[entry[0]] = entry[1]
-//         return all
-//       }, {})
-//     console.log(allEntries);
-//     return false;
-// }
-
-// onsubmit="return submitForm(this)"   Used on form Element(html)
 
 
 

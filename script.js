6201e6c709147ec3be38ebe5c98eb8202d1639ad
Radu <stroeradu03@gmail.com>
2022-01-13 10:18:52 +0200

    Refactor the code using classes

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+//ELEMENTS
+
+const addBookButton = document.querySelector('div#content button');
+const contentForm = document.querySelector('div#contentForm');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const isRead = document.querySelector('#isRead');
+const form = document.querySelector('#form');
+const cards = document.querySelector('.cards');
+
+//CLASSES
+
 class Book {
   constructor(title = 'Unknown', author = 'Unknown', pages = 0, isRead = 'false') {
     this.title = title;
@@ -16,6 +29,8 @@ class Library {
     this.books.push(book);
   }
 
+  getBook = () => new Book(title.value, author.value, pages.value, isRead.checked);
+
   removeBook = (bookTitle) => {
     this.books.forEach((book) => {
       if(book.title === bookTitle) {
@@ -23,197 +38,92 @@ class Library {
       }
     })
   }
-}
 
-const library = new Library();
-
-//ELEMENTS
-
-const addBookButton = document.querySelector('div#content button');
-const contentForm = document.querySelector('div#contentForm');
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const pages = document.querySelector('#pages');
-const isRead = document.querySelector('#isRead');
-const form = document.querySelector('#form');
-const cards = document.querySelector('.cards');
-
-
-//FUNCTIONS
-
-const showHideForm = (zIndex = 1, visibility = 'visible') => {
-  contentForm.style.zIndex = zIndex;
-  contentForm.style.visibility = visibility;
-}
-
-const getBook = () => new Book(title.value, author.value, pages.value, isRead.checked);
-
-//LOCAL STORAGE
-
-const saveLibraryToLocalStorage = (library) => {
-  localStorage.setItem('library', JSON.stringify(library.books));
-  window.location = window.location;
-}
-
-const loadLibraryFromLocalStorage = () => {
-  if(localStorage.getItem('library') === null) {
-    library.books = [];
-  } else {
-    library.books = [...JSON.parse(localStorage.getItem('library'))];
+  showLibrary = (library) => {
+    library.books.forEach((book) => {
+      const card = document.createElement('div');
+      const bookTitle = document.createElement('p');
+      const bookAuthor = document.createElement('p');
+      const bookPages = document.createElement('p');
+      const readBookButton = document.createElement('button');
+      const removeBookButton = document.createElement('button');
+    
+      bookTitle.textContent = book.title;
+      bookAuthor.textContent = book.author;
+      bookPages.textContent = book.pages;
+      removeBookButton.textContent = 'Remove';
+    
+      if (book.isRead) {
+        readBookButton.textContent = 'Read';
+        readBookButton.classList.add('green');
+      } else {
+        readBookButton.textContent = 'Not Read';
+        readBookButton.classList.add('red');
+      }
+    
+      card.classList.add('card');
+      readBookButton.classList.add('read-btn');
+      removeBookButton.classList.add('remove-btn');
+      
+      cards.appendChild(card);
+      card.appendChild(bookTitle);
+      card.appendChild(bookAuthor);
+      card.appendChild(bookPages);
+      card.appendChild(readBookButton);
+      card.appendChild(removeBookButton);
+    
+      readBookButton.addEventListener('click', () => {
+        book.isRead = !book.isRead;
+        ls.saveLibraryToLocalStorage(library);
+        console.log(book.isRead);
+      });
+      removeBookButton.addEventListener('click', () => {
+        library.removeBook(book.title)
+        ls.saveLibraryToLocalStorage(library);
+      });
+    })
   }
-};
+}
+
+class LocalStorage {
+
+  saveLibraryToLocalStorage = (library) => {
+    localStorage.setItem('library', JSON.stringify(library.books));
+    window.location = window.location;
+  }
+  
+  loadLibraryFromLocalStorage = () => {
+    if(localStorage.getItem('library') === null) {
+      library.books = [];
+    } else {
+      library.books = [...JSON.parse(localStorage.getItem('library'))];
+    }
+  };
+}
 
 //EVENT LISTENERS
 
-addBookButton.addEventListener('click', () => showHideForm());
+addBookButton.addEventListener('click', () => {
+  contentForm.style.zIndex = 1;
+  contentForm.style.visibility = 'visible';
+});
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
-  library.addBook(getBook());
-  saveLibraryToLocalStorage(library);
+  library.addBook(library.getBook());
+  ls.saveLibraryToLocalStorage(library);
   form.reset();
-  showHideForm(-1, 'hidden');
+  () => {
+    contentForm.style.zIndex = -1;
+    contentForm.style.visibility = 'hidden';
+  }
 })
 
 //RENDER BOOKS
 
-loadLibraryFromLocalStorage();
+const library = new Library();
+const ls = new LocalStorage(library);
+ls.loadLibraryFromLocalStorage();
+library.showLibrary(library);
 
-library.books.forEach((book) => {
-  const card = document.createElement('div');
-  const bookTitle = document.createElement('p');
-  const bookAuthor = document.createElement('p');
-  const bookPages = document.createElement('p');
-  const readBookButton = document.createElement('button');
-  const removeBookButton = document.createElement('button');
-
-  bookTitle.textContent = book.title;
-  bookAuthor.textContent = book.author;
-  bookPages.textContent = book.pages;
-  removeBookButton.textContent = 'Remove';
-
-  if (book.isRead) {
-    readBookButton.textContent = 'Read';
-    readBookButton.classList.add('green');
-  } else {
-    readBookButton.textContent = 'Not Read';
-    readBookButton.classList.add('red');
-  }
-
-  card.classList.add('card');
-  readBookButton.classList.add('read-btn');
-  removeBookButton.classList.add('remove-btn');
-  
-  cards.appendChild(card);
-  card.appendChild(bookTitle);
-  card.appendChild(bookAuthor);
-  card.appendChild(bookPages);
-  card.appendChild(readBookButton);
-  card.appendChild(removeBookButton);
-
-  readBookButton.addEventListener('click', () => {
-    book.isRead = !book.isRead;
-    saveLibraryToLocalStorage(library);
-    console.log(book.isRead);
-  });
-  removeBookButton.addEventListener('click', () => {
-    library.removeBook(book.title)
-    saveLibraryToLocalStorage(library);
-  });
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// addBookButton.addEventListener("click", function() {
-//   contentForm.style.zIndex = 1;
-//   contentForm.style.visibility = "visible";
-// })
-
-// const getBookFromInput = () => {
-//   const title = document.getElementById('title').value
-//   const author = document.getElementById('author').value
-//   const pages = document.getElementById('pages').value
-//   const isRead = document.getElementById('isRead').checked
-//   return new Book(title, author, pages, isRead)
-// }
-
-// const addBook = (e) => {
-//   console.log('boon');
-//   e.preventDefault();
-//   const newBook = getBookFromInput();
-//   library.addBook(newBook);
-//   saveLocal();
-//   contentForm.style.zIndex = -1;
-//   contentForm.style.visibility = "hidden";
-// }
-
-// form.onsubmit = addBook;
-
-
-// const saveLocal = () => {
-//   localStorage.setItem('library', JSON.stringify(library.books))
-// }
-
-// const restoreLocal = () => {
-//   const books = JSON.parse(localStorage.getItem('library'))
-//   if (books) {
-//     library.books = books.map((book) => JSONToBook(book))
-//   } else {
-//     library.books = []
-//   }
-// }
-
-// const JSONToBook = (book) => {
-//   return new Book(book.title, book.author, book.pages, book.isRead)
-// }
-
-// restoreLocal();
-// console.log(library.books)
 
